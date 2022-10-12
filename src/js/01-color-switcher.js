6e974c20533075d2a0a969aac51ae591ea1a69b5
RostyslavSuprun <kredlik62@gmail.com>
2022-10-12 12:11:27 +0300

    1-st task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const body =  document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+console.log(startBtn);
+console.log(stopBtn);
 
+startBtn.addEventListener("click", onStart);
+stopBtn.addEventListener("click", onStop);
+
+stopBtn.setAttribute('disabled', true);
+let timerId = null;
+
+function onStart() {
+   timerId = setInterval(() => {
+    
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+
+    startBtn.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled', true);
+};
+
+function onStop() {
+    clearInterval(timerId);
+    stopBtn.setAttribute('disabled', true);
+    startBtn.removeAttribute('disabled', true);
+};
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
