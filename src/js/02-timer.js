c67681a662295e1b2d224e11130a3754c795956d
RostyslavSuprun <kredlik62@gmail.com>
2022-10-24 23:26:59 +0300

    2.5

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,18 +2,60 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import { Ukrainian } from "flatpickr/dist/l10n/uk.js"
 flatpickr.localize(Ukrainian);
+
+const LOCALSTORAGE_KEY = "saved_time";
 require("flatpickr/dist/themes/material_green.css");
-flatpickr("#datetime-picker", {});
+flatpickr("#datetime-picker", {enableTime: true,
+    dateFormat: "Y-m-d H:i",
+  time_24hr: true,
+  defaultDate: new Date(),
+    minuteIncrement: 1,
+  minDate: Date.now(),
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      const selectedDate = selectedDates[0].getTime();
+      console.log("selectedDate =", selectedDate);
+      const result = selectedDate - Date.now();
+      const convertResult = convertMs(result);
+      console.log("result =", result);
+      localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(convertResult));
+      console.log("converted result =", convertResult);
+      startBtn.setAttribute('disabled', true);
+      changeNumbers();
+      if (result >= 0) {
+          changeNumbers();
+          startBtn.removeAttribute('disabled', true);
+          
+      }
+    },
+});
+  
 
 
 const startBtn = document.querySelector('button[data-start]');
-const seconds = document.querySelector('span[data-seconds]');
+const second = document.querySelector('span[data-seconds]');
+const minute = document.querySelector('span[data-minutes]');
+const hour = document.querySelector('span[data-hours]');
+const day = document.querySelector('span[data-days]');
 const input = document.querySelector('#datetime-picker');
-console.log(input);
 
+
+
+input.addEventListener('input', changeNumbers());
+
+
+function changeNumbers() {
+    const estimatedTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))
+    second.textContent = estimatedTime.seconds
+    minute.textContent = estimatedTime.minutes
+    hour.textContent = estimatedTime.hours
+    day.textContent = estimatedTime.days
+    console.log("estimatedTime =", estimatedTime);
+    // localStorage.removeItem(LOCALSTORAGE_KEY)
+}
 input.textContent = Date.now()
 
-startBtn.addEventListener("click", startTimer);
+// startBtn.addEventListener("click", startTimer);
 let timerId = null;
 
 const timer = {
@@ -27,16 +69,7 @@ const timer = {
     }
     
 }
-timer.start();
-
-function startTimer() {
-    timerId = setInterval(() => {
-        
-        console.log(Date.now())
-        console.log(Number(seconds.textContent+1));
-    }, 1000);
-}
-
+// timer.start();
 
 
 function convertMs(ms) {
@@ -57,3 +90,6 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+
+
+
